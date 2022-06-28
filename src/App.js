9e58e26bf2e5d5a3aa62e6b44580d0956507677e
Bo Aquarius <aquarius.bozena@gmail.com>
2022-06-28 15:19:55 +0200

    Created "Dictionary" component and set up "search" function.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="p-3">
+          <small>
+            Designed and coded by{" "}
+            <a
+              className="social-link"
+              href="https://github.com/BoAquarius"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Bo Aquarius
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
