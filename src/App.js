95343f2b616b315e4b95f79fcc6e8b1ca0811f43
Bo Aquarius <aquarius.bozena@gmail.com>
2022-07-06 13:03:08 +0200

    Added new component: Photos and integrated new API to render the corresponding photos for the searched keyword.

--- src/App.js
+++ src/App.js
@@ -6,25 +6,45 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <main>
-          <Dictionary />
-        </main>
-        <footer className="app-footer p-3">
-          <small>
-            Designed and coded by{" "}
-            <a
-              className="social-link"
-              href="https://github.com/BoAquarius"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Bo Aquarius
-            </a>
-          </small>
-        </footer>
+        <div className="App-body">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+          <main>
+            <Dictionary />
+          </main>
+          <footer className="app-footer p-3">
+            <small>
+              This app is designed and coded by {""}
+              <a
+                className="social-link"
+                href="https://laughing-carson-65f2d3.netlify.app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Bo Aquarius.
+              </a>{" "}
+              It's open-sourced on{" "}
+              <a
+                className="social-link"
+                href="https://github.com/BoAquarius"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Github
+              </a>{" "}
+              and hosted on{" "}
+              <a
+                className="social-link"
+                href="https://incomparable-malasada-4bff67.netlify.app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Netlify.
+              </a>
+            </small>
+          </footer>
+        </div>
       </div>
     </div>
   );
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
@@ -16,7 +22,16 @@ export default function Dictionary() {
 
     // documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    // documentation: https://www.pexels.com/api/documentation/?#photos-search
+    // https://stackoverflow.com/questions/44245588/how-to-send-authorization-header-with-axios
+    let pexelsApiKey =
+      "563492ad6f9170000100000120d6982b328c4ad6b4f954880510695e";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers }).then(handlePexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -36,6 +51,7 @@ export default function Dictionary() {
         </form>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
